5a9c8714fe79242d7bef87abec11d9a65a87327b
BandsWithLegends <adamosimpson21@gmail.com>
2019-02-25 21:20:05 -0800

    Adding initialize player and process whisper functions

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ let players = {};
 const defaultChatter= {
   username:undefined,
   channel:undefined,
+  makeFriend: true,
   bits:5,
   health: 0,
   attack: 0,
@@ -38,48 +39,62 @@ const defaultChatter= {
 }
 
 const introWhisper = "Thanks for joining in the game. Follow my whispers to create a creature in the game";
+const welcomeBackWhisper = "Ready to make another creature?";
 const setUpWhisper = (numBits=5) => `Your budget for stats is ${numBits} bits. Cheer with bits for more power. Whisper "food" to help the streamer or "invader" to attack them.`;
 const statsWhisper = "Choose Health, Attack, and Speed for your creature. Use numbers that add up to your budget, separated by commas. Ex: 3,1,1 for high health. 0,5,0 for extreme attack";
 const overBudgetWhisper = numBits => `Sorry! Your creature is too powerful. You are over budget by ${numBits} bits`;
 const goofWhisper = "Sorry! I didn't understand your message. Please review the instructions and try again";
 
+const initializePlayer = (userstate, channel)=> {
+  // userstate.bits contains # of bits
+  const {username} = userstate;
+  if(!players.username){
+    players.username = {...defaultChatter, username, channel};
+    players.username.bits = userstate.bits ? userstate.bits : 5;
+    client.whisper(username, introWhisper)
+  } else if (players.username.status==='done'){
+    players.username.status = 'new';
+    client.whisper(username, welcomeBackWhisper);
+  } else {
+    processWhisper(username);
+  }
+}
+
+const processWhisper = player => {
+  switch(players[username].status){
+    case 'new':
+      break;
+    case '':
+      break;
+    default:
+      break;
+  }
+}
 
 client.on("connected", function (address, port) {
-  client.whisper(myUserName, `Connected on ${address} ${port}`)
+  // client.whisper(myUserName, `Connected on ${address} ${port}`)
 });
 
 client.on("chat", (channel, userstate, message, self) => {
   message = message.toLowerCase();
-  const {username} = userstate
   if(self) return;
   // verify this later with bits and/or other necessary pieces
-  if(message.contains("!play")){
-    players[username] = defaultChatter;
-    players[username].username = username;
-    client.whisper(username, introWhisper)
+  if(message.includes("!play")){
+    initializePlayer(userstate, channel)
   }
 })
 
 client.on("cheer", (channel, userstate, message) => {
-  // Do your stuff.
-  const {username} = userstate
-  // userstate.bits contains # of bits
-  if(message.contains("!play")){
-    players[username] = defaultChatter;
-    players[username].username = username;
-    players[username].bits = userstate.bits;
-    client.whisper(username, introWhisper)
+  message = message.toLowerCase();
+  if(self) return;
+  if(message.includes("!play")){
+    initializePlayer(userstate, channel)
   }
-
 });
 
 client.on("whisper", function(from, userstate, message, self){
   if(self) return;
-  const {username} = userstate;
-  if(message.startsWith("")){
-  //
-    return;
-  }
+  processWhisper(userstate.username)
 })
 
 const verifyStats = player => {
